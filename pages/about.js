f8cf1814d0dcfc4d5821e45ce17477b0a468f863
Cronwel <noelirias@gmail.com>
2021-11-18 21:41:23 -0600

    pages created - about - projects - services

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function About() {
+  return (
+    <div>
+      About
+      <div></div>
+    </div>
+  )
+}
+
+export default About
---
+++ pages/components/Main/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+  return (
+    <div>
+      <p>I&apos;m a header</p>
+    </div>
+  )
+}
+
+export default Header
