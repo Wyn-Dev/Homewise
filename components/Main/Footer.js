2f544bcb46625745a69017e20380cbce3f2c485d
Noel Irias <noelirias@gmail.com>
2021-12-25 23:20:14 -0600

    HitList - 3 - Footer Part 2 - Added icons for social media - Added links to social media - minor changes to CSS

--- components/Main/Footer.js
+++ components/Main/Footer.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { Row, Col } from "react-bootstrap"
-
+import Image from "next/image"
+import Link from "next/link"
 function Footer() {
   return (
     <footer className='footerStyle'>
@@ -9,10 +9,27 @@ function Footer() {
         <tel>tel: (217) 259-4808</tel>
       </div>
       <div>
-        <p>
-          <icon>instagram</icon>
-          <icon>facebook</icon>
-        </p>
+        <p>FOLLOW US:</p>
+        <Link
+          href='https://www.facebook.com/homewiseconstruction/'
+          passHref={true}>
+          <Image
+            src='/images/icons/fb.png'
+            alt='facebook'
+            width={20}
+            height={20}
+          />
+        </Link>
+        <Link
+          href='https://instagram.com/homewiseconstruction/'
+          passHref={true}>
+          <Image
+            src='/images/icons/ig.png'
+            alt='facebook'
+            width={20}
+            height={20}
+          />
+        </Link>
       </div>
       <div>
         <p>
